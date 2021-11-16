6e18766654560ff0ed5e1d55409df58f1c23eeeb
mveysey <mveysey@uwo.ca>
2021-11-15 19:47:14 -0500

    DB change 1

--- initDB.js
+++ initDB.js
@@ -4,7 +4,7 @@ let conn = mysql.createConnection({
     host:'34.130.165.189',
     username:'root',
     password:'12sql34',
-    database:'doodleDB',
+    database:'doodleDB'
 });
 
 conn.connect();
